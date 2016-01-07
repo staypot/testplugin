8b646317aee7834aa3ab0eb176fc9c3bea4428f4
rommel.semira <rommel.semira@republisys.com>
2016-01-07 13:40:14 +0800

    update

---
+++ lib/republisys.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+module Repub
+  ROOT = File.expand_path File.join(File.dirname(__FILE__), '..')
+end
+
+require 'repub/genreport/rails/functions/function'
+
+# Alias for Repub-genreport
+Repub = Repub
